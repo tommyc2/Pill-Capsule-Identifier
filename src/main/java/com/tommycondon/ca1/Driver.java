e0198427b1ee16f80930cb365c8536b9b511af56
Tommy Condon <tcondon12@icloud.com>
2024-02-06 15:29:33 +0000

    Update title of application

--- src/main/java/com/tommycondon/ca1/Driver.java
+++ src/main/java/com/tommycondon/ca1/Driver.java
@@ -14,7 +14,7 @@ public class Driver extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Driver.class.getResource("gui.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Pill and Capsule Analyser");
         stage.setScene(scene);
         stage.show();
     }
